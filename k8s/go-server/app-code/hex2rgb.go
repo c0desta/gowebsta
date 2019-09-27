13c336220bce1a28507704372f89787bfd5ba17d
c0desta <tarunanand14@gmail.com>
2019-09-28 01:51:34 +0530

    helm charts for go application

---
+++ k8s/go-server/app-code/hex2rgb.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	/* "log" */
+	"github.com/gorilla/mux"
+	"github.com/lucasb-eyer/go-colorful"
+)
+
+func main() {
+
+	r := mux.NewRouter()
+	r.HandleFunc("/convert/hex-rgb/{hex}", func(w http.ResponseWriter, r *http.Request) {
+		params := mux.Vars(r)
+		hex := params["hex"]
+		c, err := colorful.Hex("#" + hex)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Fprintf(w, "RGB(%v, %v, %v)", c.R*255, c.G*255, c.B*255)
+
+	})
+
+	http.ListenAndServe(":3000", r)
+}
