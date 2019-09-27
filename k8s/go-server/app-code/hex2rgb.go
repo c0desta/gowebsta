4f355ab9cf4bfc2277f3378ae762e386eb4f831f
c0desta <tarunanand14@gmail.com>
2019-09-28 02:18:31 +0530

    prometheus  go library for custom metrics on go application

--- k8s/go-server/app-code/hex2rgb.go
+++ k8s/go-server/app-code/hex2rgb.go
@@ -7,9 +7,18 @@ import (
 	/* "log" */
 	"github.com/gorilla/mux"
 	"github.com/lucasb-eyer/go-colorful"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
+	var (
+		opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+			Name: "go_app_hex2rgb_handler_requests_total",
+			Help: "The total number of processed events",
+		})
+	)
 
 	r := mux.NewRouter()
 	r.HandleFunc("/convert/hex-rgb/{hex}", func(w http.ResponseWriter, r *http.Request) {
@@ -21,8 +30,10 @@ func main() {
 		}
 
 		fmt.Fprintf(w, "RGB(%v, %v, %v)", c.R*255, c.G*255, c.B*255)
-
+		opsProcessed.Inc()
 	})
+
+	r.Handle("/metrics", promhttp.Handler())
 
 	http.ListenAndServe(":3000", r)
 }
